42e723c6d0442109c56477e32e32878d27850ce8
lbibass <ewdries02@gmail.com>
2020-04-22 16:30:01 -0400

    Made required changes.

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 # coding=utf-8
 
-plugin_identifier = "klipper"
+plugin_identifier = "BetterKlipper"
 
-plugin_package = "octoprint_klipper"
+plugin_package = "better_octoprint_klipper"
 
-plugin_name = "OctoKlipper"
+plugin_name = "BetterOctoKlipper"
 
-plugin_version = "0.2.5.1"
+plugin_version = "0.2.5.2"
 
 plugin_description = """A plugin for OctoPrint to configure,control and monitor the Klipper 3D printer software."""
 
-plugin_author = "Martin Muehlhaeuser"
+plugin_author = "Evan D (lbibass) "
 
-plugin_author_email = "github@mmone.de"
+plugin_author_email = "lbibass1@gmail.com"
 
-plugin_url = "https://github.com/ShohninDmitriy/OctoprintKlipperPlugin"
+plugin_url = "https://github.com/lbibass/OctoprintKlipperPlugin"
 
 plugin_license = "AGPLv3"
 
